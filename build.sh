40cb5f67ae6e8760cdac280bae18588a6fb8a0f9
OneNeural <oneneural@gmail.com>
2016-01-10 08:45:02 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache --rm --force-rm=true -t combro2k/nginx-proxy-pagespeed:latest .
+docker build --no-cache --rm --force-rm=true -t engagenexecute/nginx-proxy-pagespeed:latest .
