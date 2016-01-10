e6a20273dfa2fbc0ceaad1a68e69113df3982cf5
OneNeural <oneneural@gmail.com>
2016-01-10 08:44:37 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm --name nginx-proxy-pagespeed -v /var/run/docker.sock:/tmp/docker.sock:ro combro2k/nginx-proxy-pagespeed:latest ${@}
+docker run -ti --rm --name nginx-proxy-pagespeed -v /var/run/docker.sock:/tmp/docker.sock:ro engagenexecute/nginx-proxy-pagespeed ${@}
